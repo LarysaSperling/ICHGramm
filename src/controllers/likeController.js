105c69f92a5e6a5dd7333f91bf61bc32ab2b48c1
Larysa <lorasperling@gmail.com>
2026-06-29 14:00:11 +0200

    Refactor like controller with async error handling

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,47 +1,47 @@
 import Like from "../models/Like.js";
 import Post from "../models/Post.js";
+import ApiError from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import { createNotification } from "../services/notificationService.js";
 
-const toggleLike = async (req, res) => {
-  try {
-    const existingLike = await Like.findOne({
-      user: req.user._id,
-      post: req.params.postId,
-    });
+const toggleLike = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.postId);
 
-    if (existingLike) {
-      await existingLike.deleteOne();
+  if (!post) {
+    throw new ApiError(404, "Post not found");
+  }
 
-      return res.json({
-        message: "Like removed",
-      });
-    }
+  const existingLike = await Like.findOne({
+    user: req.user._id,
+    post: req.params.postId,
+  });
 
-    await Like.create({
-      user: req.user._id,
-      post: req.params.postId,
-    });
+  if (existingLike) {
+    await existingLike.deleteOne();
 
-    const post = await Post.findById(req.params.postId);
-
-    if (post && post.author.toString() !== req.user._id.toString()) {
-      await createNotification({
-        recipient: post.author,
-        sender: req.user._id,
-        type: "like",
-        post: post._id,
-        io: req.io,
-      });
-    }
-
-    res.json({
-      message: "Post liked",
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
+    return res.json({
+      message: "Like removed",
     });
   }
-};
+
+  await Like.create({
+    user: req.user._id,
+    post: req.params.postId,
+  });
+
+  if (post.author.toString() !== req.user._id.toString()) {
+    await createNotification({
+      recipient: post.author,
+      sender: req.user._id,
+      type: "like",
+      post: post._id,
+      io: req.io,
+    });
+  }
+
+  res.json({
+    message: "Post liked",
+  });
+});
 
 export { toggleLike };
