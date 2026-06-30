3f2dfd890d69b52eaec6bfe0efcb0a13b6864c62
Larysa <lorasperling@gmail.com>
2026-06-30 10:52:10 +0200

    Add protected routes and loader

---
+++ client/src/components/ui/Loader.jsx
@@ -0,0 +1,18 @@
+const Loader = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "20px",
+        fontWeight: "600",
+      }}
+    >
+      Loading...
+    </div>
+  );
+};
+
+export default Loader;
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,21 +1,78 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Home from "../pages/Home";
 import Profile from "../pages/Profile";
+import Search from "../pages/Search";
+import Messages from "../pages/Messages";
+import Notifications from "../pages/Notifications";
+import CreatePost from "../pages/CreatePost";
 import NotFound from "../pages/NotFound";
+
+import ProtectedRoute from "../components/layout/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
 
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      <Route path="/home" element={<Home />} />
-      <Route path="/profile/:id" element={<Profile />} />
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile/:id"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <Search />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/messages"
+        element={
+          <ProtectedRoute>
+            <Messages />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/notifications"
+        element={
+          <ProtectedRoute>
+            <Notifications />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/create"
+        element={
+          <ProtectedRoute>
+            <CreatePost />
+          </ProtectedRoute>
+        }
+      />
 
       <Route path="*" element={<NotFound />} />
     </Routes>
