f725c4fce85b3b3c88824a2c04c4212185f814fe
Larysa <lorasperling@gmail.com>
2026-07-03 08:41:01 +0200

    Add followers and following fields to user model

--- src/models/User.js
+++ src/models/User.js
@@ -38,6 +38,20 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     timestamps: true,
